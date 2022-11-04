b9aee8229c4fba3e2ae0c52c7caae5f0e770fc85
Art Pau <arturas.study@gmail.com>
2022-11-05 00:42:04 +0100

    slider fixed

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -2,27 +2,55 @@ import batman from "../images/batman.jpeg"
 import coda from "../images/coda.jpeg"
 import dumbledore from "../images/dumbledore.jpeg"
 import arrow from "../images/arrow.png"
-import { useState } from "react"
+import { useState, useRef } from "react"
+import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos';
+import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
+
 const Home = () => {
-    const [slide, setSlide] = useState(0);
-    
+    const [slide, setSlide] = useState(1);
+    const ref = useRef();
+
+    const prev = () => {
+        if(slide > 1) {
+            ref.current.style.transform = `translateX(-${(slide - 2) * 1440}px)`;
+            setSlide(prev => prev - 1);
+        } else {
+            ref.current.style.transform = `translateX(-${(3 - 1) * 1440}px)`;
+            setSlide(3);
+        }
+    }
+
+    const next = () => {
+        if(slide < 3) {
+            ref.current.style.transform = `translateX(-${slide * 1440}px)`;
+            setSlide(prev => prev + 1);
+        } else {
+            ref.current.style.transform = 'translateX(0px)';
+            setSlide(1);
+        }
+    }
+
   return (
     <div className="frame">
+        <div>
+
+        </div>
+        <div ref={ref} className="slider">
+            <img className="image" src={dumbledore} alt="" />
+            <img className="image" src={batman} alt="" />
+            <img className="image" src={coda} alt="" />
+        </div>
         <div className="arrows">
             <div>
-                <img src={arrow} className="arrow left" alt="" />
+                <ArrowForwardIosIcon className="arrow left" onClick={prev}/>
             </div>
             <div>
-                <img src={arrow} className="arrow right" alt="" onClick={() => setSlide(slide + 1)}/>
+                <ArrowForwardIosIcon className="arrow" onClick={next}/>
+
             </div>
         </div>
         <div className="bottom">
         </div>
-        <div className="slider" style={{ transform: `translateX(-${slide * 1440}px)` }}>
-            <img className="image" src={batman} alt="" />
-            <img className="image" src={dumbledore} alt="" />
-        </div>
-        
     </div>
   )
 }
